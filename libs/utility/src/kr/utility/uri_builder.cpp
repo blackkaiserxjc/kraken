26bf7d3c961fc6d9142ca5ff68b403dc021b750e
kaiser <blackkaiserxjc@gmail.com>
2021-07-25 20:51:20 +0800

    删除uribuilder,暂时无需求.

---
+++ libs/utility/src/kr/utility/uri_builder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kaiser on 2021/7/24.
+//
+
+#include "uri_builder.h"
---
+++ libs/utility/src/kr/utility/uri_builder.h
@@ -0,0 +1,12 @@
+//
+// Created by kaiser on 2021/7/24.
+//
+
+#ifndef KRAKEN_URI_BUILDER_H
+#define KRAKEN_URI_BUILDER_H
+
+class uri_builder
+{
+};
+
+#endif //KRAKEN_URI_BUILDER_H
